6adb53419ea13a19a691bdaa9aa2bd6c7785cae6
henrique campanha <henriqueoc@msn.com>
2021-12-29 20:45:25 +0000

    Initial Commit

---
+++ LearningApp/LearningAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningAppApp.swift
+//  LearningApp
+//
+//  Created by henrique campanha on 29/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct LearningAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
